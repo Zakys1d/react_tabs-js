84b031509ed624f0e89ac43e22bdf0a93e98fc28
Maksym Huk <maksym.h1009@gmail.com>
2026-06-06 20:49:37 +0300

    result

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -19,7 +19,7 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
               }}
               data-cy="TabLink"
             >
-              {tab.label}
+              {tab.title}
             </a>
           </li>
         ))}
